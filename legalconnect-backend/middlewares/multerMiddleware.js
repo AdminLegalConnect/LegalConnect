39eaa79e5ab658762d5d1fff8a517c2a4f22d945
AdminLegalConnect <ad.legalconnect@gmail.com>
2025-05-19 18:57:29 +0200

    fichierok

--- legalconnect-backend/middlewares/multerMiddleware.js
+++ legalconnect-backend/middlewares/multerMiddleware.js
@@ -21,12 +21,17 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = [
-    "image/jpeg",
-    "image/png",
-    "application/pdf",
-    "application/msword",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  ];
+  "application/pdf",
+  "application/msword",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "image/jpeg",
+  "image/png",
+  "application/zip",
+  "text/plain",
+  "application/vnd.ms-excel", // .xls
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" // .xlsx
+];
+
 
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
